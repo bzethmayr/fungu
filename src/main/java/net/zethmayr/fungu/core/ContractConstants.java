d33d6d0379145283cfb67075e213383f7ff1edbf
unknown <gribnitt@gmail.com>
2023-06-08 21:01:10 -0500

    fix javadoc

--- src/main/java/net/zethmayr/fungu/core/ContractConstants.java
+++ src/main/java/net/zethmayr/fungu/core/ContractConstants.java
@@ -2,11 +2,17 @@ package net.zethmayr.fungu.core;
 
 import static net.zethmayr.fungu.core.ExceptionFactory.becauseConstantsOnly;
 
+/**
+ * Declares constants used with the {@link org.jetbrains.annotations.Contract} annotation.
+ */
 public final class ContractConstants {
 
     private ContractConstants() {
         throw becauseConstantsOnly();
     }
 
+    /**
+     * Indicates that this unary method always returns the argument passed, {@literal i.e.} is an identity method.
+     */
     public static final String IDENTITY = "_->param1";
 }
